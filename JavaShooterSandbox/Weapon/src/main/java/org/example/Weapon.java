9a9e9356a1e561544de818057a9c0eb76ae685ae
oskar566 <oschr20@student.sdu.dk>
2022-05-05 11:31:22 +0200

    WeaponModuleUpdate

--- JavaShooterSandbox/Weapon/src/main/java/org/example/Weapon.java
+++ JavaShooterSandbox/Weapon/src/main/java/org/example/Weapon.java
@@ -3,12 +3,11 @@ package org.example;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.physics.box2d.Body;
 import org.example.data.Entity;
+import org.example.data.parts.HealthPart;
 
 public class Weapon extends Entity
 {
-    public Weapon(float x, float y, int width, int height, Sprite sprite, Body body)
-    {
-        super(x,y,width,height,sprite,body);
-
+    public Weapon(float x, float y, float speed, int width, int height, Body body, Sprite sprite, HealthPart healthPart){
+        super(x, y, speed, width, height, body, sprite, healthPart);
     }
 }
--- JavaShooterSandbox/Weapon/src/main/java/org/example/WeaponController.java
+++ JavaShooterSandbox/Weapon/src/main/java/org/example/WeaponController.java
@@ -2,15 +2,22 @@ package org.example;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import org.example.data.Entity;
 import org.example.data.GameWorld;
+import org.example.helper.AssetLoader;
+import org.example.helper.Const;
+import org.example.helper.LibWorld;
+import org.example.spi.IBulletService;
 import org.example.spi.IEntityProcessingService;
+import org.example.spi.IGamePluginService;
+import org.openide.util.Lookup;
 import org.openide.util.lookup.ServiceProvider;
 import org.openide.util.lookup.ServiceProviders;
-
-import java.io.File;
-import java.util.Random;
 
 @ServiceProviders(value = {@ServiceProvider(service = IEntityProcessingService.class)})
 public class WeaponController implements IEntityProcessingService
@@ -18,10 +25,9 @@ public class WeaponController implements IEntityProcessingService
     @Override
     public void update(GameWorld world, SpriteBatch batch)
     {
-        if(Gdx.input.isKeyPressed(Input.Keys.SPACE))
-        {
-            System.out.println("Weapon is shooting");
-        }
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+        System.out.println("Weapon is shooting");
+    }
     }
 
     @Override
