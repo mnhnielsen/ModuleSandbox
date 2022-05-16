1f9edd7a789ef1c2395a9ce81477d9ef02acf27d
MathiasHansen95 <maha520@student.sdu.dk>
2022-05-16 23:28:27 +0200

    Tiledmap visual update

---
+++ JavaShooterSandbox/application/TX Tileset Stone Ground.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="TX Tileset Stone Ground" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <properties>
+  <property name="blocked" value="blocked"/>
+ </properties>
+ <image source="TX Tileset Stone Ground.png" width="256" height="256"/>
+</tileset>
--- JavaShooterSandbox/application/tileset.tsx
+++ JavaShooterSandbox/application/tileset.tsx
@@ -1,5 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.4" name="tileset" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <properties>
+  <property name="walkable" value="walkable"/>
+ </properties>
  <image source="TX-Tileset-Grass.png" width="256" height="256"/>
  <tile id="0">
   <properties>
