4894ac2a738a75990c61c3d3b361174eddec6705
MathiasHansen95 <maha520@student.sdu.dk>
2022-05-19 13:56:08 +0200

    enemyNavLayer
    enemyNavLayer

--- JavaShooterSandbox/application/TX Tileset Stone Ground.tsx
+++ JavaShooterSandbox/application/TX Tileset Stone Ground.tsx
@@ -1,8 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.4" name="TX Tileset Stone Ground" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <properties>
-  <property name="blocked" value="blocked"/>
- </properties>
  <image source="TX Tileset Stone Ground.png" width="256" height="256"/>
  <tile id="56">
   <properties>
